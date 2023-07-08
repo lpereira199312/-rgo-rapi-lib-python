d76643e7411bfbd532eeef50b988405c847f754f
lpereira199312 <94242809+lpereira199312@users.noreply.github.com>
2023-07-08 13:59:46 +0100

    Add files via upload

---
+++ rapi.py
@@ -0,0 +1,50 @@
+# pip install request
+import requests
+# pip install hmac
+import hmac
+# pip install hashlib
+import hashlib
+
+import urllib.parse
+
+# def keys receive and store apikey and secret key
+def keys(apikey,secretkey):
+    global api_key
+    api_key = apikey
+    global secret_key
+    secret_key = secretkey
+
+#def generate_signature calculate signature
+def generate_signature(secret_key,data):
+    key_sec=bytes(secret_key,encoding='utf-8')
+    data=bytes(data,encoding='utf-8')
+    signature = hmac.new(key_sec,data,digestmod=hashlib.sha256).hexdigest()
+    return signature
+
+#def format_data format data for sending in request
+def format_data (data):
+    data=urllib.parse.urlencode(data)
+    return data
+
+#def request contruct and process request process
+def request(metodo,path,data):
+    requests.packages.urllib3.disable_warnings()
+    base_url = "https://api.reactioon.com:1357"
+    url = base_url + path
+    headers= {
+        'X-RTN-KEY': f'{api_key}',
+        'X-RTN-SIGNATURE': '',
+    }
+    if metodo == 'get':
+        signature = generate_signature(secret_key,data)
+        headers['X-RTN-SIGNATURE']=f'{signature}'
+        response = requests.get( headers=headers, verify=False)
+        return response
+    if metodo == 'post':
+        content = data
+        data = format_data(data)
+        signature = generate_signature(secret_key,data)
+        headers['X-RTN-SIGNATURE'] = f'{signature}'
+        headers['Content-type'] = 'application/x-www-from-urlencoded'
+        response = requests.post(data=data,headers=headers, verify=False)
+        return response
---
+++ test.py
@@ -0,0 +1,16 @@
+import rapi
+
+apikey="sua api"
+secretkey="sua secret key"
+
+# metodo post get price for get snapshot in exchange binance for currency usdt 
+
+rapi.keys(apikey,secretkey)
+r = rapi.request("post","/api/v2/snapshots/cost",{'exchange':'binance','currency':'USDT'})
+print(r.text)
+
+# metodo get get assets in your watchlist
+
+rapi.keys(apikey,secretkey)
+r =rapi.request("get","/api/v2/watchlist/assets","")
+print(r.text)
