a382a40678784b751a2b5cc53e12f61bbd98b15c
lpereira199312 <94242809+lpereira199312@users.noreply.github.com>
2023-07-08 17:39:33 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import rapi
 
-apikey={sua api}
-secretkey={sua secret key}
+apikey={your-api-key}
+secretkey={your-secret-key}
 
 # metodo post get price for get snapshot in exchange binance for currency usdt 
 
