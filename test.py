2834b8da6aad3329d366daafb3f9db16b7c547b1
lpereira199312 <94242809+lpereira199312@users.noreply.github.com>
2023-07-08 18:51:01 +0100

    Update test.py

--- test.py
+++ test.py
@@ -3,14 +3,14 @@ import rapi
 apikey={your-api-key}
 secretkey={your-secret-key}
 
-# metodo post get price for get snapshot in exchange binance for currency usdt 
+# method POST get price for get snapshot in exchange binance for currency usdt 
 
 rapi.keys(apikey,secretkey)
-r = rapi.request("post","/api/v2/snapshots/cost",{'exchange':'binance','currency':'USDT'})
+r = rapi.request("POST","/api/v2/snapshots/cost",{'exchange':'binance','currency':'USDT'})
 print(r.text)
 
-# metodo get get assets in your watchlist
+# method GET get assets in your watchlist
 
 rapi.keys(apikey,secretkey)
-r =rapi.request("get","/api/v2/watchlist/assets","")
+r =rapi.request("GET","/api/v2/watchlist/assets","")
 print(r.text)
